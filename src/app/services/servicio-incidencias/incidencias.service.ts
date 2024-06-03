21fbc2df50d3cff1a0d73cdcbb1f1c7539960eec
Javier Tatay <jtatay2@iesfuentesanluis.org>
2024-06-03 23:58:45 +0200

    estoy desesperao

--- src/app/services/servicio-incidencias/incidencias.service.ts
+++ src/app/services/servicio-incidencias/incidencias.service.ts
@@ -1,33 +1,42 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { AuthService } from '../servicio-auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IncidenciasService {
 
-  constructor(private http: HttpClient) { }
+  private apiUrl = 'https://intraivo.online/public/api/incidencias';
+
+  constructor(private http: HttpClient, private authService: AuthService) {}
+
+  private getHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token_usuario');  // Obtener el token del localStorage
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`  // Usar el token en el encabezado de autorización
+    });
+  }
 
   obtenerIncidenciasEmpleado(id: number): Observable<any> {
-    return this.http.get(`https://intraivo.online/public/api/incidencias/mostrarIncidenciasEmpleado/${id}`);
+    return this.http.get(`${this.apiUrl}/mostrarIncidenciasEmpleado/${id}`, { headers: this.getHeaders() });
   }
 
   crearIncidencia(incidencia: any): Observable<any> {
-    return this.http.post('https://intraivo.online/public/api/incidencias/guardar', incidencia);
+    return this.http.post(`${this.apiUrl}/guardar`, incidencia, { headers: this.getHeaders() });
   }
 
-  editarIncidencia(incidencia: any): Observable<any>{
-    return this.http.put(`https://intraivo.online/public/api/incidencias/actualizar/${incidencia.id}`, incidencia);
-    
+  editarIncidencia(incidencia: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/actualizar/${incidencia.id}`, incidencia, { headers: this.getHeaders() });
   }
 
   obtenerIncidenciaPorId(id: number): Observable<any> {
-    return this.http.get(`https://intraivo.online/public/api/incidencias/buscar/${id}`);
+    return this.http.get(`${this.apiUrl}/buscar/${id}`, { headers: this.getHeaders() });
   }
 
-  eliminarIncidencia(id: number): Observable<any>{
-    return this.http.delete(`https://intraivo.online/public/api/incidencias/borrar/${id}`);
+  eliminarIncidencia(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/borrar/${id}`, { headers: this.getHeaders() });
   }
-
 }
